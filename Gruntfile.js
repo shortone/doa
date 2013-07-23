4dc7350575fed17239556d22c8c9ecce83479c32
AlphaHydrae <hydrae.alpha@gmail.com>
2013-07-23 22:17:16 +0200

    Grunt watch for specs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,46 @@ module.exports = function(grunt) {
     jasmine_node: {
       projectRoot: 'spec/server',
       requirejs: false,
-      forceExit: true
+      forceExit: true,
+      growl: true
+    },
+
+    karma: {
+
+      options: {
+        configFile: 'karma.conf.js',
+        singleRun: true
+      },
+
+      dev: {
+      },
+
+      ci: {
+        browsers: [ 'PhantomJS' ]
+      }
+    },
+
+    watch: {
+
+      browser: {
+        files: [ 'lib/assets/js/**/*.js', 'lib/assets/js/**/*.coffee' ],
+        tasks: [ 'karma:ci' ],
+        atBegin: true
+      },
+
+      server: {
+        files: [ 'lib/**/*.js', '!lib/assets/**' ],
+        tasks: [ 'jasmine_node' ],
+        atBegin: true
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', [ 'jshint', 'jasmine_node' ]);
 };
