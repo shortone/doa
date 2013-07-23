bf4080660e9b3311799e2a9f3a7b9b8b900234e1
AlphaHydrae <hydrae.alpha@gmail.com>
2013-07-23 22:23:56 +0200

    Npm test script for travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,5 +53,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', [ 'jshint', 'jasmine_node' ]);
+  grunt.registerTask('default', [ 'jshint', 'karma:ci', 'jasmine_node' ]);
 };
